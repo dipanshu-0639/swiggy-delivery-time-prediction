46f804b9b06c15e28e2fbe06a8e40fc32d5d43b7
Dipanshu <dipanshu639722@gmail.com>
2026-06-22 22:21:38 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Build a Machine Learning model that predict food delivery time in minutes.',
+    author='Dipanshu',
+    license='',
+)
